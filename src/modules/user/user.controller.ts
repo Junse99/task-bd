0911357a35338fcfb5189576b01cf4a9a195f793
Sebastian Obregón <sebasobregon99@hotmail.com>
2021-04-10 01:55:04 -0500

    Feature(Entities): entities created

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  constructor(private readonly userService: UserService){}
+
+  @Get('/all')
+  async getAllUsers(){
+    return await this.userService.getAllUsers()
+  }
+
+}
